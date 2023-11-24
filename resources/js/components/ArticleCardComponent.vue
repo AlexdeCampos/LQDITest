6f01cfbb3ed13c181d8d15ecbc3771d610b58c3b
AlexdeCampos <alexcampos.dev@gmail.com>
2023-11-24 17:14:49 -0300

    Lead page article component

---
+++ resources/js/components/ArticleCardComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="article-card">
+        <img :src="card.image" class="article-image" />
+        <h4>{{ card.title }}</h4>
+        <p>{{ card.content }}</p>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        card: {
+            image: { default: "", required: true },
+            title: { default: "", required: true },
+            content: { default: "", required: true },
+        },
+    },
+};
+</script>
