e9a4cf63e3199a14b726bd447e0db5a8a83f6bb2
AlexdeCampos <alexcampos.dev@gmail.com>
2023-11-25 01:19:47 -0300

    project routes

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LeadsController;
 use Illuminate\Support\Facades\Route;
 
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'auth');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum');
+    Route::post('/register', 'store');
+});
+
+
 Route::controller(LeadsController::class)->group(function () {
-    Route::post('/leads/{id}', 'sendEmail');
-    Route::get('/leads', 'list');
+    Route::post('/leads/{id}', 'sendEmail')->middleware('auth:sanctum');
+    Route::get('/leads', 'list')->middleware('auth:sanctum');
     Route::post('/leads', 'store');
 });
+
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('lead-page');
 });
+Route::get('/leads', function () {
+    return view('lead-list');
+});
